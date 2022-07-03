b299f36c6851ccabbe1dc8066d4b98a8f340c87c
Usuario <javichua.a@hotmail.com>
2022-07-03 19:38:06 +0200

    feat: create new biome

--- src/main/java/net/mcreator/duskcarft/DuskcarftMod.java
+++ src/main/java/net/mcreator/duskcarft/DuskcarftMod.java
@@ -26,6 +26,8 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.network.FriendlyByteBuf;
 
+import net.mcreator.duskcarft.init.DuskcarftModBiomes;
+
 import java.util.function.Supplier;
 import java.util.function.Function;
 import java.util.function.BiConsumer;
@@ -42,6 +44,8 @@ public class DuskcarftMod {
 	public DuskcarftMod() {
 
 		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+
+		DuskcarftModBiomes.REGISTRY.register(bus);
 
 	}
 
