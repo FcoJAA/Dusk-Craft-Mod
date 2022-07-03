477c2a2a4c440a188fcfd959ed1a758e65ae573e
Usuario <javichua.a@hotmail.com>
2022-07-03 20:35:46 +0200

    feat: upgrade dusk-biome

--- src/main/java/net/mcreator/duskcarft/world/biome/DuskBiomeBiome.java
+++ src/main/java/net/mcreator/duskcarft/world/biome/DuskBiomeBiome.java
@@ -20,46 +20,62 @@ import net.minecraft.world.level.biome.Climate;
 import net.minecraft.world.level.biome.BiomeSpecialEffects;
 import net.minecraft.world.level.biome.BiomeGenerationSettings;
 import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.biome.AmbientParticleSettings;
+import net.minecraft.world.entity.MobCategory;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.util.valueproviders.ConstantInt;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.Music;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.data.worldgen.placement.PlacementUtils;
 import net.minecraft.data.worldgen.features.VegetationFeatures;
 import net.minecraft.data.worldgen.features.FeatureUtils;
 import net.minecraft.data.worldgen.BiomeDefaultFeatures;
+import net.minecraft.core.particles.ParticleTypes;
 
 import java.util.List;
 
 public class DuskBiomeBiome {
-	public static final Climate.ParameterPoint PARAMETER_POINT = new Climate.ParameterPoint(Climate.Parameter.span(-0.142857142857f, 0.142857142857f),
-			Climate.Parameter.span(-0.142857142857f, 0.142857142857f), Climate.Parameter.span(0.367142857143f, 0.652857142857f),
-			Climate.Parameter.span(0.657142857143f, 0.942857142857f), Climate.Parameter.point(0),
-			Climate.Parameter.span(0.646638115319f, 0.932352401033f), 0);
+	public static final Climate.ParameterPoint PARAMETER_POINT = new Climate.ParameterPoint(Climate.Parameter.span(-0.714285714286f, 0.714285714286f),
+			Climate.Parameter.span(-0.114285714286f, 1.314285714286f), Climate.Parameter.span(-0.204285714286f, 1.224285714286f),
+			Climate.Parameter.span(0.085714285714f, 1.514285714286f), Climate.Parameter.point(0),
+			Climate.Parameter.span(0.07520954389f, 1.503780972462f), 0);
 
 	public static Biome createBiome() {
-		BiomeSpecialEffects effects = new BiomeSpecialEffects.Builder().fogColor(-13434778).waterColor(-16764109).waterFogColor(329011)
-				.skyColor(-13434778).foliageColorOverride(-10092442).grassColorOverride(-10092442).build();
+		BiomeSpecialEffects effects = new BiomeSpecialEffects.Builder().fogColor(-13434829).waterColor(-16764109).waterFogColor(329011)
+				.skyColor(-13434829).foliageColorOverride(-13421773).grassColorOverride(-13421773)
+				.backgroundMusic(new Music(new SoundEvent(new ResourceLocation("ambient.soul_sand_valley.additions")), 12000, 24000, true))
+				.ambientParticle(new AmbientParticleSettings(ParticleTypes.MYCELIUM, 0.01f)).build();
 		BiomeGenerationSettings.Builder biomeGenerationSettings = new BiomeGenerationSettings.Builder();
 		biomeGenerationSettings.addFeature(GenerationStep.Decoration.VEGETAL_DECORATION, PlacementUtils.register("duskcarft:tree_dusk_biome",
 				FeatureUtils.register("duskcarft:tree_dusk_biome", Feature.TREE,
-						new TreeConfiguration.TreeConfigurationBuilder(BlockStateProvider.simple(Blocks.OAK_LOG.defaultBlockState()),
-								new StraightTrunkPlacer(4, 2, 0), BlockStateProvider.simple(Blocks.OAK_LEAVES.defaultBlockState()),
+						new TreeConfiguration.TreeConfigurationBuilder(BlockStateProvider.simple(Blocks.DARK_OAK_WOOD.defaultBlockState()),
+								new StraightTrunkPlacer(6, 2, 0), BlockStateProvider.simple(Blocks.SPRUCE_LEAVES.defaultBlockState()),
 								new BlobFoliagePlacer(ConstantInt.of(2), ConstantInt.of(0), 3), new TwoLayersFeatureSize(1, 0, 1)).ignoreVines()
 								.build()),
-				List.of(CountPlacement.of(1), InSquarePlacement.spread(), SurfaceWaterDepthFilter.forMaxDepth(0),
+				List.of(CountPlacement.of(5), InSquarePlacement.spread(), SurfaceWaterDepthFilter.forMaxDepth(0),
 						PlacementUtils.HEIGHTMAP_OCEAN_FLOOR, PlacementUtils.filteredByBlockSurvival(Blocks.OAK_SAPLING), BiomeFilter.biome())));
 		biomeGenerationSettings.addFeature(GenerationStep.Decoration.VEGETAL_DECORATION,
 				PlacementUtils.register("duskcarft:grass_dusk_biome", VegetationFeatures.PATCH_GRASS,
-						List.of(NoiseThresholdCountPlacement.of(-0.8D, 5, 4), InSquarePlacement.spread(), PlacementUtils.HEIGHTMAP_WORLD_SURFACE,
+						List.of(NoiseThresholdCountPlacement.of(-0.8D, 5, 5), InSquarePlacement.spread(), PlacementUtils.HEIGHTMAP_WORLD_SURFACE,
 								BiomeFilter.biome())));
 		biomeGenerationSettings.addFeature(GenerationStep.Decoration.VEGETAL_DECORATION,
-				PlacementUtils.register("duskcarft:flower_dusk_biome", VegetationFeatures.FLOWER_DEFAULT, List.of(CountPlacement.of(4),
+				PlacementUtils.register("duskcarft:flower_dusk_biome", VegetationFeatures.FLOWER_DEFAULT, List.of(CountPlacement.of(1),
+						RarityFilter.onAverageOnceEvery(32), InSquarePlacement.spread(), PlacementUtils.HEIGHTMAP, BiomeFilter.biome())));
+		biomeGenerationSettings.addFeature(GenerationStep.Decoration.VEGETAL_DECORATION,
+				PlacementUtils.register("duskcarft:brown_mushroom_dusk_biome", VegetationFeatures.PATCH_BROWN_MUSHROOM, List.of(CountPlacement.of(1),
+						RarityFilter.onAverageOnceEvery(32), InSquarePlacement.spread(), PlacementUtils.HEIGHTMAP, BiomeFilter.biome())));
+		biomeGenerationSettings.addFeature(GenerationStep.Decoration.VEGETAL_DECORATION,
+				PlacementUtils.register("duskcarft:red_mushroom_dusk_biome", VegetationFeatures.PATCH_RED_MUSHROOM, List.of(CountPlacement.of(1),
 						RarityFilter.onAverageOnceEvery(32), InSquarePlacement.spread(), PlacementUtils.HEIGHTMAP, BiomeFilter.biome())));
 		BiomeDefaultFeatures.addDefaultCarversAndLakes(biomeGenerationSettings);
 		BiomeDefaultFeatures.addDefaultOres(biomeGenerationSettings);
 		BiomeDefaultFeatures.addSurfaceFreezing(biomeGenerationSettings);
 		MobSpawnSettings.Builder mobSpawnInfo = new MobSpawnSettings.Builder();
+		mobSpawnInfo.addSpawn(MobCategory.CREATURE, new MobSpawnSettings.SpawnerData(EntityType.SHULKER, 1, 1, 1));
 		return new Biome.BiomeBuilder().precipitation(Biome.Precipitation.RAIN).biomeCategory(Biome.BiomeCategory.NONE).temperature(0.5f)
-				.downfall(0.5f).specialEffects(effects).mobSpawnSettings(mobSpawnInfo.build()).generationSettings(biomeGenerationSettings.build())
-				.build();
+				.downfall(0.7999999999999999f).specialEffects(effects).mobSpawnSettings(mobSpawnInfo.build())
+				.generationSettings(biomeGenerationSettings.build()).build();
 	}
 
 	public static void init() {
