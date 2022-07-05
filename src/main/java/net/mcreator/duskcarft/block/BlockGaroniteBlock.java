a0acd75d8210a85cc9dd33f899c81707c68eafe0
Usuario <javichua.a@hotmail.com>
2022-07-05 20:29:47 +0200

    fix: particles

--- src/main/java/net/mcreator/duskcarft/block/BlockGaroniteBlock.java
+++ src/main/java/net/mcreator/duskcarft/block/BlockGaroniteBlock.java
@@ -1,27 +1,20 @@
 
 package net.mcreator.duskcarft.block;
 
-import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.api.distmarker.Dist;
-
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.item.TieredItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.core.BlockPos;
-import net.minecraft.client.Minecraft;
 
 import net.mcreator.duskcarft.init.DuskcarftModBlocks;
 
-import java.util.Random;
 import java.util.List;
 import java.util.Collections;
 
@@ -48,24 +41,5 @@ public class BlockGaroniteBlock extends Block {
 		if (!dropsOriginal.isEmpty())
 			return dropsOriginal;
 		return Collections.singletonList(new ItemStack(DuskcarftModBlocks.BLOCK_GARONITE.get()));
-	}
-
-	@OnlyIn(Dist.CLIENT)
-	@Override
-	public void animateTick(BlockState blockstate, Level world, BlockPos pos, Random random) {
-		super.animateTick(blockstate, world, pos, random);
-		Player entity = Minecraft.getInstance().player;
-		int x = pos.getX();
-		int y = pos.getY();
-		int z = pos.getZ();
-		for (int l = 0; l < 4; ++l) {
-			double x0 = x + random.nextFloat();
-			double y0 = y + random.nextFloat();
-			double z0 = z + random.nextFloat();
-			double dx = (random.nextFloat() - 0.5D) * 0.7000000000000001D;
-			double dy = (random.nextFloat() - 0.5D) * 0.7000000000000001D;
-			double dz = (random.nextFloat() - 0.5D) * 0.7000000000000001D;
-			world.addParticle(ParticleTypes.PORTAL, x0, y0, z0, dx, dy, dz);
-		}
 	}
 }
